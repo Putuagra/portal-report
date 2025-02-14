53402f961d19db3abb97a97c83011f73715cf6f2
putuagra <iputuagra@gmail.com>
2025-02-14 16:14:25 +0700

    feat: add check day and data last month in pdf

--- app/core/presentation.py
+++ app/core/presentation.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from core.data import check_type
 from typing import Any
+import pandas as pd
 
 
 # def format_number(num):
@@ -11,6 +12,21 @@ from typing import Any
 #         return f"{num / 1000:.1f}K"
 #     else:
 #         return str(num)
+
+
+def check_date_day(df_obj):
+    df_obj = pd.to_datetime(df_obj)
+    day_date = df_obj.iloc[0].day_name()
+    return day_date
+
+
+def find_specific_day(df_obj, target_day):
+    df_obj = pd.to_datetime(df_obj)
+
+    matching_dates = [date for date in df_obj if date.day_name() == target_day]
+
+    if matching_dates:
+         return matching_dates[0]
 
 
 def bar_chart(ax, x_axis, df_obj, width, color: str, label: dict[str, str], offset=0):
@@ -33,9 +49,9 @@ def bar_chart(ax, x_axis, df_obj, width, color: str, label: dict[str, str], offs
         )
 
 
-def line_chart(ax, axis, df_obj, color: str, label: str):
+def line_chart(ax, axisx, df_obj, color: str, label: str):
     ax.plot(
-        axis,
+        axisx,
         df_obj,
         marker="o",
         linestyle="-",
@@ -54,7 +70,7 @@ def add_annotation(ax, text, x, y, isBar=True):
         fontsize=10,
         fontweight="bold",
         color="red" if isBar else "black",
-    ) # -> create annotation for chart
+    )  # -> create annotation for chart
     # plt.scatter([x], [y], color="red", marker="*", s=100) # -> for star logo
 
 
@@ -62,10 +78,11 @@ def generate_vertical_bar(
     df: dict[str, Any], filename: str, label: dict[str, str], selected_type: str
 ):
     variable = check_type(selected_type)
-    chart = {"fontsize": 12, "fontsize_title": 14, "fontweight": "bold", "pad": 10}
-    df_this_month = df["this_month"].head(10).reset_index(drop=True) # -> get first 10 data
-    max_index_thismonth = df_this_month[label].idxmax() # -> get index for line chart
-    max_value_thismonth = df_this_month[label].max() # -> get max value for line chart
+    chart = {"fontsize": 12, "fontsize_title": 14, "fontweight": "bold", "pad": 20}
+    df_this_month = df["this_month"].reset_index(drop=True)  # -> get first 10 data
+    first_day_this_month = check_date_day(df_this_month[f"{variable['REQ_TRX']} Date"])
+    max_index_thismonth = df_this_month[label].idxmax()  # -> get index for line chart
+    max_value_thismonth = df_this_month[label].max()  # -> get max value for line chart
     fig, ax = plt.subplots(figsize=(15, 6))
     width = 0.2
     x = np.arange(len(df_this_month[f"{variable['REQ_TRX']} Date"]))
@@ -124,15 +141,26 @@ def generate_vertical_bar(
         max_value_thismonth,
     )
     if not df["last_month"].empty:
-        df_last_month = df["last_month"].head(10).reset_index(drop=True)
-        x_mapped = np.arange(len(df_last_month[f"{variable['REQ_TRX']} Date"]))
+        df_last_month = df["last_month"].reset_index(drop=True)
+        day_last_month = find_specific_day(
+            df_last_month[f"{variable['REQ_TRX']} Date"], first_day_this_month
+        )
+        df_last_month[f"{variable['REQ_TRX']} Date"] = pd.to_datetime(
+            df_last_month[f"{variable['REQ_TRX']} Date"]
+        )
+        df_filtered = df_last_month[
+            df_last_month[f"{variable['REQ_TRX']} Date"] >= day_last_month
+        ].reset_index(drop=True)
+        x_mapped = np.arange(len(df_filtered))
+        df_filter = df_filtered[label]
 
-        max_index_lastmonth = df_last_month[label].idxmax()
-        max_value_lastmonth = df_last_month[label].max()
+
+        max_index_lastmonth = df_filtered[label].idxmax()
+        max_value_lastmonth = df_filtered[label].max()
         line_chart(
             ax,
             x_mapped,
-            df_last_month[label],
+            df_filter,
             "deepskyblue",
             "Total Request Last Month",
         )
@@ -145,15 +173,16 @@ def generate_vertical_bar(
         )
         # Add another label for last month data
         ax_top = ax.twiny()
-        ax_top.set_xlim(ax.get_xlim()) # -> make sure both axes are in the same line
-        ax_top.set_xticks(x_mapped) # -> how much ticks for date
+        ax_top.set_xlim(ax.get_xlim())  # -> make sure both axes are in the same line
+        ax_top.set_xticks(x_mapped)  # -> how much ticks for date
         ax_top.set_xlabel(
             f"{variable['REQ_TRX']} Date (Last Month)",
             fontsize=chart["fontsize"],
             fontweight=chart["fontweight"],
+            loc="left",
         )
         ax_top.set_xticklabels(
-            df_last_month[f"{variable['REQ_TRX']} Date"], rotation=45
+            df_filtered[f"{variable['REQ_TRX']} Date"].dt.strftime("%Y-%m-%d"), rotation=45
         )
 
     # Add label
@@ -162,8 +191,9 @@ def generate_vertical_bar(
         fontsize=12,
         color="red",
         fontweight=chart["fontweight"],
+        loc="left",
     )
-    ax.set_xticks(x) # -> how much ticks for date
+    ax.set_xticks(x)  # -> how much ticks for date
     ax.set_xticklabels(df_this_month[f"{variable['REQ_TRX']} Date"], rotation=45)
     ax.legend(bbox_to_anchor=(1, 1), loc="upper left")
     plt.tight_layout()
